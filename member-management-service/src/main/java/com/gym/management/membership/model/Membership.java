5af7041070cd899725931760ed56f6d92556e919
Aniruddh Panicker <Aniruddhrpanicker@gmail.com>
2025-01-31 13:28:54 +0530

    Fixed some bugs in membership service

--- member-management-service/src/main/java/com/gym/management/membership/model/Membership.java
+++ member-management-service/src/main/java/com/gym/management/membership/model/Membership.java
@@ -3,6 +3,8 @@ package com.gym.management.membership.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -19,35 +21,41 @@ import lombok.Data;
 @Entity
 @Table(name = "memberships")
 public class Membership {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    @NotNull(message = "Member is mandatory")
-    @OneToOne
-    @JoinColumn(name = "member_id")
-    private Member member;
+	@NotNull(message = "Member is mandatory")
+	@OneToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
 
-    @NotNull(message = "Membership type is mandatory")
-       private MembershipType membershipType;
+	@NotNull(message = "Membership type is mandatory")
+	private MembershipType membershipType;
 
-    @NotNull(message = "Membership status is mandatory")
-    @Enumerated(EnumType.STRING)
-    private MembershipStatus status;
+	@NotNull(message = "Membership status is mandatory")
+	@Enumerated(EnumType.STRING)
+	private MembershipStatus status;
 
-    @NotNull(message = "Start date is mandatory")
-    private LocalDate startDate;
+	@NotNull(message = "Start date is mandatory")
+	private LocalDate startDate;
 
-    @NotNull(message = "End date is mandatory")
-    private LocalDate endDate;
+	@NotNull(message = "End date is mandatory")
+	private LocalDate endDate;
 
-    private LocalDateTime createdAt = LocalDateTime.now();
+	private LocalDateTime createdAt = LocalDateTime.now();
 
-    public enum MembershipType {
-        BASIC, PREMIUM
-    }
+	public enum MembershipType {
+		BASIC, PREMIUM
+	}
 
-    public enum MembershipStatus {
-        ACTIVE, INACTIVE
-    }
+	public enum MembershipStatus {
+		ACTIVE, INACTIVE
+	}
+
+	@Override
+	public String toString() {
+		return "Membership{" + "id=" + id + ", membershipType=" + membershipType + ", status=" + status + ", startDate="
+				+ startDate + ", endDate=" + endDate;
+	}
 }
